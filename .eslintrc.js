fe9e3570df6b37571e31c94fe4fd278bdeee9842
piercenovo <piercenovoah@gmail.com>
2022-05-27 17:52:05 -0500

    init: inital project setup

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'no-template-curly-in-string': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/jsx-key': 'off'
+  }
+}
